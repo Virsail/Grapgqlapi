d203583e485f141a9ba6b9c281bd4bba05fe4c7a
virsail <virsaileric@gmail.com>
2021-06-20 00:36:18 +0300

    install and import bodyParserGraphQL

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-const express = ('express');
-const expressGraphQl = ('express-graphql');
+const express = require ('express');
+const expressGraphQl = require ('express-graphql').graphqlHTTP
 const port = 4800 ;
 const app = express()
+const schema = require ('./schema/schema');
 
-app.use('/graphql',expressGraphQl({
+import { bodyParserGraphQL } from 'body-parser-graphql'
+
+app.use('/graphql',expressGraphQl,bodyParserGraphQL({
     schema,
-    graphql:true
+    graphiql:true
 }))
 app.listen (port, () => {
     console.log(`listening to port ${port}`)
