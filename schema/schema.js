e86f529ad1b10156bc44d9302727875726fe03fc
virsail <virsaileric@gmail.com>
2021-06-19 18:24:24 +0300

    have a set of data array to test api query

---
+++ schema/schema.js
@@ -0,0 +1,10 @@
+const graphql = require ('graphql');
+const _ = require ('lodash');
+
+const users = [
+    {"id":"0702644583", "firstname": "Cherryl", "age":20},
+    {"id":"0115068741", "firstname": "Virsail", "age":20},
+
+]
+
+const 
