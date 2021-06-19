d36d113017f865fc9ec9a16f64bc572dd9951cc1
virsail <virsaileric@gmail.com>
2021-06-19 18:34:20 +0300

    define the Schema structure

--- schema/schema.js
+++ schema/schema.js
@@ -1,10 +1,18 @@
 const graphql = require ('graphql');
 const _ = require ('lodash');
 
+const {GraphQLObjectType,GraphQLString} = graphql
+
 const users = [
     {"id":"0702644583", "firstname": "Cherryl", "age":20},
     {"id":"0115068741", "firstname": "Virsail", "age":20},
 
 ]
 
-const 
+const UserType = new graphql.GraphQLObjectType({
+name: 'User',
+fields:{
+    id:{type:GraphQLString}
+    
+}
+})
