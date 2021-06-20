0d016a862dd4adaca342f27894e34ca49df9914a
virsail <virsaileric@gmail.com>
2021-06-20 20:52:29 +0300

    add data types to schema file

--- schema/schema.js
+++ schema/schema.js
@@ -5,7 +5,9 @@ const {GraphQLObjectType,GraphQLString,GraphQLInt} = graphql
 
 const users = [
     {"id":"1", "firstname": "Mercy", "secondname": "Mukoya", "efficiency delta": 1.3, "nps delta": 1.2, "efficiency": 96, "reported issues": 3, "age":20},
-    {"id":"2", "firstname": "Virsail", "age":20},
+    {"id":"2", "firstname": "Kennedy", "secondname": "Ayako", "efficiency delta": 1.8, "nps delta": 1.8, "efficiency": 92, "reported issues": 6, "age":20},
+    {"id":"3", "firstname": "Stephanie", "secondname": "Tomsett", "efficiency delta": 2.7, "nps delta": 2.0, "efficiency": 58, "reported issues": 1, "age":30},
+    {"id":"4", "firstname": "Faith", "secondname": "Kityo", "efficiency delta": 2.8, "nps delta": 2.5, "efficiency": 74, "reported issues": 8, "age":42},
 
 ]
 
