0d30f1237bf96558546102f1c61072c57f78b652
virsail <virsaileric@gmail.com>
2021-06-21 03:55:32 +0300

    push to heroku

--- schema/schema.js
+++ schema/schema.js
@@ -30,6 +30,7 @@ const visits = [
     {"location": "Kosovo", "hospital": "Clinic Kosovo", "times": 4 },
     {"location": "Kakamega", "hospital": "Oasis", "times": 46 },
     {"location": "Kalibu", "hospital": "Alib", "times": 16 },
+    {"location": "India", "hospital": "ulius", "times": 9 },
     {"location": "Mukurukwanjenga", "hospital": "Mukuru level5", "times": 26 }
 ] 
 
